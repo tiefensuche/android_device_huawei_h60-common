38518bda34fd61f2d35bf04e6024b15064c20757
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-01-18 15:25:13 +0100

    h60: add nfc access configs

--- h60-common.mk
+++ h60-common.mk
@@ -123,9 +123,9 @@ PRODUCT_COPY_FILES += \
 
 # NFCEE access control
 ifeq ($(TARGET_BUILD_VARIANT),user)
-	NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access.xml
+	NFCEE_ACCESS_PATH := $(COMMON_PATH)/rootdir/system/etc/nfcee_access.xml
 else
-	NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access_debug.xml
+	NFCEE_ACCESS_PATH := $(COMMON_PATH)/rootdir/system/etc/nfcee_access_debug.xml
 endif
 
 PRODUCT_COPY_FILES += \
