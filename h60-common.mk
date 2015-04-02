0f3a59ba826e8c18b2a48533c9c8d1bace49f319
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-04-02 20:30:33 +0200

    h60: remove languages-full.mk

--- h60-common.mk
+++ h60-common.mk
@@ -28,8 +28,6 @@ COMMON_PATH := device/huawei/h60-common
 
 DEVICE_PACKAGE_OVERLAYS := $(COMMON_PATH)/overlay
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
